5e6626bdf82a58ca70db81c23ac583fda6f88516
Ismar Sarac <ss2ismar@hotmail.com>
2022-05-09 14:15:37 -0700

    added some html stuff, broke the grid,fixed the grid, then broke it again....

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const container=document.querySelector('#grid-container');
 
-function makeRows(rows,cols){
-    container.style.setProperty('--grid-rows',rows);
-    container.style.setProperty('--grid-cols',cols);
-    for (c=0;c<(rows*cols);c++){
+function makeRows(gridNum){
+    let gridArea=gridNum*gridNum;
+    for (c=0;c<(gridArea);c++){
         let cell = document.createElement('div');
-        //cell.innerText=(c+1);
+        container.style.gridTemplateColumns=`repeat(${gridNum},1fr)`;
+        container.style.gridTemplateRows=`repeat(${gridNum},1fr)`;
+        // cell.innerText=(c+1);
         container.appendChild(cell).className='grid-item';
     };
 };
+makeRows(16);
 
-makeRows(16,16);
